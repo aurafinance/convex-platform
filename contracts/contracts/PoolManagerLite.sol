1ca4d453c6415b456674b0e39c152e4a616657a5
phijfry <phijfry@protonmail.com>
2023-03-30 16:54:36 +0100

    feat: init lite

---
+++ contracts/contracts/PoolManagerLite.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+/** 
+ * @title   PoolManagerLite
+ * @author  AuraFinance 
+ */
+contract PoolManagerLite {
+    constructor() public {}
+}
