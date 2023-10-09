eb6c54c117af676a70ed163f9a5b6507b661a3f8
0xahtle7 <0xahtle7@gmail.com>
2023-10-09 15:32:42 +0200

    docs: adds natspec to stashToken

--- contracts/contracts/StashToken.sol
+++ contracts/contracts/StashToken.sol
@@ -12,26 +12,42 @@ interface IERC20Metadata {
     function symbol() external view returns (string memory); 
 }
 
+/**
+ * @title StashToken
+ * @notice StashToken is not ERC20 compliant. 
+ * @dev StashToken is not ERC20 compliant. 
+ *      it represents a token that can be minted only by the stash contract, and transfered by the reward pool.
+ */
 contract StashToken is ReentrancyGuardUpgradeable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
     uint256 public constant MAX_TOTAL_SUPPLY = 1e38;
-    address public immutable stash;
 
+    // State variables
+    address public immutable stash;
     address public operator;
     address public rewardPool;
     address public baseToken;
     bool public isValid;
     bool public isImplementation;
-
     uint256 internal _totalSupply;
 
+    /**
+     * @dev Constructor function
+     * @param _stash Address of the stash contract.
+     */
     constructor(address _stash) public {
         stash = _stash;
         isImplementation = true;
     }
 
+    /**
+     * @dev Initialization function to set operator, reward pool, and base token addresses.
+     * @param _operator Address of the operator.
+     * @param _rewardPool Address of the reward pool.
+     * @param _baseToken Address of the base token.
+     */
     function init(
         address _operator,
         address _rewardPool,
@@ -47,23 +63,43 @@ contract StashToken is ReentrancyGuardUpgradeable {
         isValid = true;
     }
 
+    /**
+     * @dev Get the concatenated name of the StashToken.
+     * @return The concatenated name of the StashToken.
+     */
     function name() external view returns (string memory) {
         return string(abi.encodePacked("Stash Token ", IERC20Metadata(baseToken).name()));
     }
 
+    /**
+     * @dev Get the concatenated symbol of the StashToken.
+     * @return The concatenated symbol of the StashToken.
+     */
     function symbol() external view returns (string memory) {
         return string(abi.encodePacked("STASH-", IERC20Metadata(baseToken).symbol()));
     }
 
+    /**
+     * @dev Set the validity status of the StashToken.
+     * @param _isValid Boolean indicating the validity status.
+     */
     function setIsValid(bool _isValid) external {
         require(msg.sender == IDeposit(operator).owner(), "!owner");
         isValid = _isValid;
     }
 
+    /**
+     * @dev Get the total supply of the StashToken.
+     * @return The total supply of the StashToken.
+     */
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
 
+    /**
+     * @dev Mint new StashToken tokens, only callable  by the stash contract.
+     * @param _amount Amount of tokens to mint.
+     */
     function mint(uint256 _amount) external nonReentrant {
         require(msg.sender == stash, "!stash");
         require(_totalSupply.add(_amount) < MAX_TOTAL_SUPPLY, "totalSupply exceeded");
@@ -72,6 +108,12 @@ contract StashToken is ReentrancyGuardUpgradeable {
         IERC20(baseToken).safeTransferFrom(msg.sender, address(this), _amount);
     }
 
+    /**
+     * @dev Transfer StashToken tokens to a specified address. Only callable by the reward pool contract.
+     * @param _to Address to which tokens are transferred.
+     * @param _amount Amount of tokens to transfer. 
+     * @return A boolean indicating whether the transfer was successful.
+     */
     function transfer(address _to, uint256 _amount) public nonReentrant returns (bool) {
         require(msg.sender == rewardPool, "!rewardPool");
         require(_totalSupply >= _amount, "amount>totalSupply");
